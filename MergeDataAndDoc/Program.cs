e0baf23c1d488e9002fbb847bb452799555dcf03
Jimmy <jimparbparamy2@gmail.com>
2016-03-17 14:09:00 +0800

    防呆：沒有輸入

--- MergeDataAndDoc/Program.cs
+++ MergeDataAndDoc/Program.cs
@@ -11,26 +11,35 @@ namespace MergeDataAndDoc
     {
         static void Main(string[] args)
         {
-            string inputFileName = "defaultInput.txt";
-            string outputFileName = "defaultOutput.txt";
+            string inputFileName = "";
+            string outputFileName = "";
+
             if (args.Length == 2)
             {
-                inputFileName = args[0];
-                outputFileName = args[1];
-                Console.WriteLine("The file has been loaded");
-            }
-
-            using (StreamReader inputFile = new StreamReader(inputFileName))
-            using(StreamWriter outputFile = new StreamWriter(outputFileName))
-            {
-                string line; //test
-                while((line = inputFile.ReadLine()) != null)
                 {
-                    string outputLine = "***" + line;
-                    Console.WriteLine("Write line: " + outputLine);
-                    outputFile.WriteLine(outputLine);
+                    inputFileName = args[0];
+                    outputFileName = args[1];
+                    Console.WriteLine("The files has been loaded");
+                }
+
+                using (StreamReader inputFile = new StreamReader(inputFileName))
+                using (StreamWriter outputFile = new StreamWriter(outputFileName))
+                {
+                    string line; //test
+                    while ((line = inputFile.ReadLine()) != null)
+                    {
+                        string outputLine = "***" + line;
+                        Console.WriteLine("Write line: " + outputLine);
+                        outputFile.WriteLine(outputLine);
+                    }
                 }
             }
+            else
+            {
+                Console.WriteLine("The files loading is fail");
+            }
+
+            Console.Read();
         }
     }
 }
